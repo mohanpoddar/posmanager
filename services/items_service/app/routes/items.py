4a654d7c82c7d4d72f30ec0c4328dbc324019484
Mohan Poddar <contactme.mohan@gmail.com>
2025-07-20 23:08:21 +0530

    update code

--- services/items_service/app/routes/items.py
+++ services/items_service/app/routes/items.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import Session
 from app.models.db import SessionLocal, Item
 from app.models.schema import ItemCreate, ItemOut
 from app.crud import items as crud
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import OperationalError
+from app.models.db import engine
+
 
 router = APIRouter()
 
@@ -21,3 +25,12 @@ def list_items(db: Session = Depends(get_db)):
 @router.post("/items", response_model=ItemOut)
 def add_item(item: ItemCreate, db: Session = Depends(get_db)):
     return crud.create_item(item, db)
+
+@router.get("/health/db")
+def check_database_connection():
+    try:
+        with engine.connect() as conn:
+            conn.execute("SELECT 1")
+        return JSONResponse(status_code=200, content={"status": "ok", "message": "Database connected"})
+    except OperationalError as e:
+        return JSONResponse(status_code=500, content={"status": "error", "message": str(e)})
